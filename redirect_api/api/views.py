25e9c5acb297cb347c6c87dd4779ac8db7ccfeb1
NaokiMatsumoto <9nmatsumoto@gmail.com>
2024-02-14 18:22:19 +0900

    change view

--- redirect_api/api/views.py
+++ redirect_api/api/views.py
@@ -17,15 +17,17 @@ class LessonViewSet(viewsets.ReadOnlyModelViewSet):
         # 現在の日付を取得
         now = datetime.now()
         # 日付に基づいてリダイレクトURLに使用する日付の部分を決定
-        day = '01' if now.day < 16 else '16'
-        url_date = now.strftime('%Y%m') + day
-        
+        day = '01'
+        if 11 <= now.day < 20:
+            day = '11'
+        elif now.day >= 20:
+            day = '21'
+        url_date = now.strftime('%Y%m') + day   
         # オブジェクトを取得
         instance = self.get_object()
         
         # リダイレクトURLを決定。instance.urlがNoneの場合は、instance.default_urlを使用
         redirect_url = instance.url or instance.default_url
-        
         # redirect_urlがNoneの場合は404エラーを発生
         if redirect_url is None:
             raise Http404("ページが見つかりませんでした。")
