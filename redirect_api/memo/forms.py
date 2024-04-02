4e0545c198f87c63333f8384dba16bfaf12119b8
NaokiMatsumoto <9nmatsumoto@gmail.com>
2024-04-02 18:04:15 +0900

    bug fix

--- redirect_api/memo/forms.py
+++ redirect_api/memo/forms.py
@@ -5,3 +5,7 @@ class MemoForm(forms.ModelForm):
     class Meta:
         model = Memo
         fields = ['title', 'content']
+
+    def __init__(self, *args, **kwargs):
+      super(MemoForm, self).__init__(*args, **kwargs)
+      self.fields["content"].required = False
